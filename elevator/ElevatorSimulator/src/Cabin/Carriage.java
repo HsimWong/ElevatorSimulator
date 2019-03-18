cb3a716a94cc7fa27f861b7c661cd2bb8878e1a0
root <root@vultr.guest>
2019-03-18 11:21:18 +0800

    Virtual

--- elevator/ElevatorSimulator/src/Cabin/Carriage.java
+++ elevator/ElevatorSimulator/src/Cabin/Carriage.java
@@ -5,14 +5,37 @@ public class Carriage{
 	private int capNumPerson;
 	private int curNumPerson;
 	private int currentFloor;
-	
-	public Carriage(int capacity, int floorNum) {
+	private int maxFloor;
+	private int minFloor;
+	public Carriage(int capacity, int maxFloor, int minFloor) {
+		if (maxFloor <= 0 || minFloor >=0) {
+			throw exception
+		
 		this.cabinPanel = new CabinPanel(floorNum);
 		this.capNumPerson = capacity;
 		this.curNumPerson = 0;
 		this.currentFloor = 1;
+		this.maxFloor = maxFloor;
+		this.minFloor = minFloor;
+
+
 	}
 	
+	/* direction is indicated with boolean value.
+	 * Up = true, Down = false */
+	private moveUp1Floor(boolean direction) {
+		if (direction) {
+			if currentFloor
+			this.currentFloor++;
+		} else {
+			this.currentFloor--;
+		}
+	}	
+
+	public go2Floor(int floorNum){
+		
+	}
+
 //	public String toString() {
 //		
 //	}
